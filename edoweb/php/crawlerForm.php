60b18be005a39c3237c4361a209dc0cdf9f7bdec
Heinzelmann <inkuss@github.com>
2019-07-19 12:03:29 +0200

    deutsche Texte nicht in php ändern   - einige Änderungen deutscher Texte wieder rückgängig gemacht, da nicht sauber codiert   - Zurückgestellt bis zur besseren Lösung; JIRA-Ticket angelegt

--- edoweb/php/crawlerForm.php
+++ edoweb/php/crawlerForm.php
@@ -91,14 +91,14 @@ function edoweb_basic_crawler_form($form, &$form_state, $entity) {
     console_log('active='.@$conf['active']);
     $form['active'] = array(
         '#type' => 'checkbox',
-        '#title' => t('Aktiv'),
+        '#title' => t('Active'),
         '#default_value' => @$conf['active'] == null ? true : @$conf['active'],
         '#weight' => 20,
     );
     
     $form['startDate'] = array(
         '#type' => 'date',
-        '#title' => t('Datum des 1. Crawls'),
+        '#title' => t('Start Date'),
         '#default_value' => isset($conf['startDate'])
         ?  date_parse($conf['startDate'])
         : array(),
@@ -125,10 +125,10 @@ function edoweb_basic_crawler_form($form, &$form_state, $entity) {
     
     $form['robotsPolicy'] = array(
         '#type' => 'radios',
-        '#title' => t('Robots-Regeln'),
+        '#title' => t('Robots'),
         '#options' => array(
-            'ignore' => t('Ignorieren'),
-            'obey' => t('Befolgen'),
+            'ignore' => t('Ignore'),
+            'obey' => t('Obey'),
         ),
         '#default_value' => @$conf['robotsPolicy'] == null ? 'ignore' : @$conf['robotsPolicy'] == 'classic' ? 'ignore' : @$conf['robotsPolicy'],
         '#required' => TRUE,
