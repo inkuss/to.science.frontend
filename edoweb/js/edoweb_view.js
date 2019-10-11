bcae5c74924feb4a730b4c739f53d71dd9756308
Andres Quast <quast@hbz-nrw.de>
2019-10-11 16:05:22 +0200

    changed sorting from jquery to elasticsearch via facet order reverse_term

--- edoweb/js/edoweb_view.js
+++ edoweb/js/edoweb_view.js
@@ -59,15 +59,15 @@
       });
       
       // Sort & load listnav
-      $('#issued', context).each(function() {
-        var listnav = $(this);
-        var listitems = listnav.children('li').get();
-        listitems.sort(function(b, a) {
-           return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
-        })
-        $.each(listitems, function(idx, itm) { listnav.append(itm); });
-        //$(this).listnav();
-      });
+      //$('#issued', context).each(function() {
+      //  var listnav = $(this);
+      //  var listitems = listnav.children('li').get();
+      //  listitems.sort(function(b, a) {
+      //     return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
+      //  })
+      //  $.each(listitems, function(idx, itm) { listnav.append(itm); });
+      //  //$(this).listnav();
+      //});
 
     }
 
