c6aca56b41f51e25f8de135955096cf25d5401ee
Ingolf Kuss <kuss@hbz-nrw.de>
2025-09-24 20:17:41 +0200

    Integrate Browsertrix in crawler form

--- edoweb/php/crawlerForm.php
+++ edoweb/php/crawlerForm.php
@@ -201,6 +201,7 @@ function edoweb_basic_crawler_form($form, &$form_state, $entity) {
         '#options' => array(
             'heritrix' => t('heritrix'),
             'wpull' => t('wpull'),
+            'browsertrix' => t('browsertrix'),
         ),
         '#default_value' => @$conf['crawlerSelection'] == null ? 'wpull' : @$conf['crawlerSelection'],
         '#required' => FALSE,
@@ -214,7 +215,7 @@ function edoweb_basic_crawler_form($form, &$form_state, $entity) {
     console_log('issetDeepness='.(@$conf['deepness']!==null));
     console_log('deepness='.@$conf['deepness']);
     
-    if( @$conf['crawlerSelection'] == 'wpull' ) {
+    if( @$conf['crawlerSelection'] == 'wpull' || @$conf['crawlerSelection'] == 'browsertrix' ) {
         
         $form['urlsExcluded'] = array(
             '#type' => 'fieldset',
@@ -457,7 +458,7 @@ function edoweb_basic_crawler_form_submit($form, &$form_state) {
     $conf['robotsPolicy'] = $form_state['values']['robotsPolicy'];
     $conf['notices'] = $form_state['values']['notices'];
     $conf['crawlerSelection'] = $form_state['values']['crawlerSelection'];
-    if( $conf['crawlerSelection'] == 'wpull' ) {
+    if( @$conf['crawlerSelection'] == 'wpull' || @$conf['crawlerSelection'] == 'browsertrix' ) {
         if( isset($form_state['values']['urlExcluded00']) && $form_state['values']['urlExcluded00'] != '') {
             $conf['urlsExcluded'] = array($form_state['values']['urlExcluded00']);
         }
